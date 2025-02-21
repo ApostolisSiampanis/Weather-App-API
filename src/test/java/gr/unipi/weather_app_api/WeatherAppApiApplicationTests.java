8a66db0e533d800e14e171a66381848850b2ed77
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2025-02-21 21:32:51 +0200

    Initial Commit

---
+++ src/test/java/gr/unipi/weather_app_api/WeatherAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.unipi.weather_app_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAppApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
