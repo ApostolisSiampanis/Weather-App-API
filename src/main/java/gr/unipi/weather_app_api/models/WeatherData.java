51d1a254521cbaecbb0798d4148d0e1b9a766cbc
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2025-03-03 22:17:17 +0200

    Create WeatherData entity

---
+++ src/main/java/gr/unipi/weather_app_api/models/WeatherData.java
@@ -0,0 +1,44 @@
+package gr.unipi.weather_app_api.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "weather_data", schema = "public")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class WeatherData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "city", nullable = false, length = 50)
+    private String city;
+
+    @Column(name = "temperature", nullable = false)
+    private int temperature;
+
+    @Column(name = "humidity", nullable = false)
+    private int humidity;
+
+    @Column(name = "wind_speed", nullable = false)
+    private int windSpeed;
+
+    @Column(name = "uv_index", nullable = false)
+    private int uvIndex;
+
+    @Column(name = "weather_description", nullable = false)
+    private String weather_description;
+
+    @Column(name = "timestamp", nullable = false)
+    private LocalDateTime timestamp;
+}
