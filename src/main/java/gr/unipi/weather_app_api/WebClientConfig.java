f8c0dcc33fd637e1dfab98a55223155805061c17
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2025-03-03 22:17:33 +0200

    Create WebClientConfig

---
+++ src/main/java/gr/unipi/weather_app_api/WebClientConfig.java
@@ -0,0 +1,21 @@
+package gr.unipi.weather_app_api;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Value("${weather.api.base-url}")
+    private String baseUrl; // Base URL for weather API
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl(baseUrl)
+                .build();
+    }
+
+}
